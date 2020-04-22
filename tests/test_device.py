3ad065521c2172fcd7fad7f037be1805fffbe520
NR5P <orangev8zcar@gmail.com>
2020-04-21 20:05:24 -0500

    test finished

--- tests/test_device.py
+++ tests/test_device.py
@@ -6,11 +6,15 @@ from device import Device
 
 class TestDevice(unittest.TestCase):
     def setUp(self):
-        #TODO: left off adding test
-        pass
+        self.timeStamp8601 = "2020-04-22T06:08:02+0000"
+        self.datetimeFromTimeStamp = datetime.datetime.strptime(self.timeStamp8601,"%Y-%m-%dT%H:%M:%S%z")
+        self.secondsOn = 180
+        self.timedeltaFromSecondsOn = datetime.timedelta(seconds=self.secondsOn)
+        self.timesToConvertDict = {self.timeStamp8601:self.secondsOn}
 
-    def test_convertToDatetimeTimeDeltaDict():
-        pass
+    def test_convertToDatetimeTimeDeltaDict(self):
+        self.dictToTest = {self.datetimeFromTimeStamp:self.timedeltaFromSecondsOn}
+        self.assertEqual(Device.convertToDatetimeTimedeltaDict(self.timesToConvertDict),self.dictToTest)
 
 #    def test_convertDatetimeToTimeDelta(self):
 #        timeDeltaToTest = datetime.timedelta(hours=2,minutes=42,seconds=3)
